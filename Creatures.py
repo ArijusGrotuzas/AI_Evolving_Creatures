6f04334c40bca6e158ab4ff44e5e4f37a6eac29a
Arijus Grotuzas <50104866+ArijusGrotuzas@users.noreply.github.com>
2021-03-18 18:46:24 +0100

    Added a lanscape generator and reproduction function to creatures

--- Creatures.py
+++ Creatures.py
@@ -3,14 +3,10 @@ import string
 
 
 class individual:
-    def __init__(self, size, max_val):
-        self.chromosome = self.rand_chromosome(size, max_val)
+    def __init__(self, chromosome):
+        self.chromosome = chromosome
         self.fitness = self.asses_fitness(self.chromosome)
-        self.name = self.rand_name(size)
-
-    @staticmethod
-    def rand_chromosome(size, max_val):
-        return [random.randint(1, max_val) for _ in range(size)]
+        self.name = self.rand_name(3)
 
     @staticmethod
     def asses_fitness(arr):
@@ -27,13 +23,13 @@ class individual:
 
 
 class population:
-    def __init__(self, size):
-        self.individuals = self.random_individuals(size)
+    def __init__(self, size, chromo_size, max_value):
+        self.individuals = self.random_individuals(size, chromo_size, max_value)
         self.generation_age = 0
 
     @staticmethod
-    def random_individuals(size):
-        return [individual(3, 10) for _ in range(size)]
+    def random_individuals(size, chromo_size, max_value):
+        return [individual([random.randint(1, max_value) for _ in range(chromo_size)]) for _ in range(size)]
 
 
 def get_most_fit(populous):
@@ -46,17 +42,40 @@ def get_most_fit(populous):
     return most_fit
 
 
+def mutate(singula, max_val):
+    singula.chromosome[random.randint(0, len(singula.chromosome) - 1)] = random.randint(-2, max_val)
+
+
+def reproduce(parent_one, parent_two):
+    n = len(parent_one.chromosome)
+    c = (random.randint(1, n - 1))
+    offspring = individual(parent_one.chromosome[0:c] + parent_two.chromosome[c:n])
+    return offspring
+
+
 def main():
-    pop = population(10)
+    pop = population(100, 3, 10)
 
     for i in pop.individuals:
         print(i.name)
         print(i.fitness)
         print("\n")
 
-    print("\nmost fit: ")
-    maximus_fitnesus = get_most_fit(pop)
-    print(maximus_fitnesus.name)
+    print("\nfirst parent")
+    print(pop.individuals[20].chromosome)
+    print("fitness")
+    print(pop.individuals[20].fitness)
+
+    print("\nsecond parent")
+    print(pop.individuals[42].chromosome)
+    print("fitness")
+    print(pop.individuals[42].fitness)
+
+    child = reproduce(pop.individuals[20], pop.individuals[42])
+    print("\noffspring")
+    print(child.chromosome)
+    print("fitness")
+    print(child.fitness)
 
     del pop
 
---
+++ Landscape.py
@@ -0,0 +1,81 @@
+import math
+import numpy
+import cv2
+
+
+def cosine_interpolation(a, b, t):
+    ft = t * math.pi
+    f = (1 - math.cos(ft)) * 0.5
+    return a * (1 - f) + b * f
+
+
+def random_gradient(x, y):
+    random = 2920.0 * math.sin(x * 21942.0 + y * 171324.0 + 8912.0) * math.cos(x * 23157.0 * y * 217832.0 + 9758.0)
+    return math.cos(random), math.sin(random)
+
+
+def dot_grid_gradient(ix, iy, x, y):
+    # Get gradient from integer coordinates
+    (gradientX, gradientY) = random_gradient(ix, iy)
+
+    # Compute the distance vector
+    dx = x - float(ix)
+    dy = y - float(iy)
+
+    # Compute the dot-product
+    return dx * gradientX + dy * gradientY
+
+
+def perlin(x, y):
+    # Determine grid cell coordinates
+    x0 = int(x)
+    x1 = x0 + 1
+    y0 = int(y)
+    y1 = y0 + 1
+
+    # Determine interpolation weights
+    # Could also use higher order polynomial/s-curve here
+    sx = x - float(x0)
+    sy = y - float(y0)
+
+    # Interpolate between grid point gradients
+    n0 = dot_grid_gradient(x0, y0, x, y)
+    n1 = dot_grid_gradient(x1, y0, x, y)
+    ix0 = cosine_interpolation(n0, n1, sx)
+
+    n0 = dot_grid_gradient(x0, y1, x, y)
+    n1 = dot_grid_gradient(x1, y1, x, y)
+    ix1 = cosine_interpolation(n0, n1, sx)
+
+    return cosine_interpolation(ix0, ix1, sy)
+
+
+def create_landscape(size, threshold, amplitude):
+    landscape = [[0 for _ in range(size)] for __ in range(size)]
+
+    for i in range(size):
+        for j in range(size):
+            n = perlin(i * amplitude, j * amplitude)
+
+            n += 1.0
+            n /= 2.0
+
+            if n > threshold:
+                landscape[i][j] = 1.0
+            else:
+                landscape[i][j] = 0.0
+
+    return landscape
+    # perlin_noise = numpy.array(landscape)
+    # print(landscape)
+    # cv2.imshow('perlin', cv2.resize(perlin_noise, (800, 800), interpolation=cv2.INTER_AREA))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+def main():
+    land = create_landscape(50, 0.45, 0.1)
+
+
+if __name__ == '__main__':
+    main()
